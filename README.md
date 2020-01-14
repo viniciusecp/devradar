ad785dab7886462f5d4e751d5eb059c91e34fa3b
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2020-01-14 13:59:49 -0300

    Added backend

---
+++ README.md
@@ -0,0 +1,3 @@
+# Semana Omnistack #10 | Rocketseat
+
+- Desenvolvimento de uma aplicação utilizando a stack **Node.js**, **ReactJS** e **React Native**.
---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/devradar", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
